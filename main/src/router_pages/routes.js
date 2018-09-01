2a8dbfd54cd60471e4c893ce0425cf7509f8ca5f
Ivanq <imachug@yandex.ru>
2018-09-01 09:54:22 +0300

    Play with a friend/Random

--- main/src/router_pages/routes.js
+++ main/src/router_pages/routes.js
@@ -1,5 +1,6 @@
 import Home from "./home/home.vue";
 import Play from "./play/play.vue";
+import PlayGame from "./play/play-game.vue";
 
 export default vue => [
 	{
@@ -13,6 +14,12 @@ export default vue => [
 		path: "play",
 		controller: () => {
 			vue.currentView = Play;
+		}
+	},
+	{
+		path: "play/:game",
+		controller: () => {
+			vue.currentView = PlayGame;
 		}
 	}
 ];

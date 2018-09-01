090cef60c1ff1821377897ace9025362b72d990f
Ivanq <imachug@yandex.ru>
2018-09-01 08:59:17 +0300

    More auth providers

--- main/src/zero.js
+++ main/src/zero.js
@@ -11,7 +11,14 @@ import ZeroDB from "zero-dev-lib/ZeroDB";
 const zeroDB = new ZeroDB(zeroPage);
 
 import ZeroAuth from "zero-dev-lib/ZeroAuth";
-const zeroAuth = new ZeroAuth(zeroPage);
+const zeroAuth = new ZeroAuth(zeroPage, [
+	"zeroid.bit",
+	"kaffie.bit",
+	"cryptoid.bit",
+	"peak.id",
+	"zeroverse.bit",
+	"kxoid.bit"
+]);
 zeroPage.auth = zeroAuth;
 
 export {zeroPage, zeroDB, zeroFS, zeroAuth};

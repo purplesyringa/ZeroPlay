4a66111350623a73aa5d230ab1e0b89fc54c5dd1
Ivanq <imachug@yandex.ru>
2018-09-01 10:18:53 +0300

    Add Users.userNameToAddress

--- main/src/libs/users.js
+++ main/src/libs/users.js
@@ -1,4 +1,4 @@
-import {zeroPage, zeroFS} from "@/zero";
+import {zeroPage, zeroFS, zeroDB} from "@/zero";
 
 export default new class Users {
 	// Register current user as <username>
@@ -22,5 +22,35 @@ export default new class Users {
 		const authAddress = siteInfo.auth_address;
 
 		return await zeroFS.fileExists(`data/users/${authAddress}/data.json`);
+	}
+
+	// Transform username to auth_address
+	async userNameToAddress(username) {
+		let results;
+		if(username.indexOf("@") > -1) {
+			// It's likely cert_user_id
+			results = await zeroDB.query(`
+				SELECT * FROM json
+				WHERE cert_user_id = :cert_user_id
+			`, {
+				cert_user_id: username
+			});
+		} else {
+			// It's likely username
+			results = await zeroDB.query(`
+				SELECT * FROM json
+				WHERE username = :username
+			`, {
+				username
+			});
+		}
+
+		if(results.length === 0) {
+			throw new Error(`There is no user with username ${username}`);
+		} else if(results.length > 1) {
+			throw new Error(`There are several users with username ${username} -- use username@zeroid.bit to choose`);
+		}
+
+		return results[0].directory.replace("users/", "");
 	}
 };

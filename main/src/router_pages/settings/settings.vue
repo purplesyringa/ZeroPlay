bd2f568dbcff0d0b2054a5e6f45b99f4b46cc001
Ivanq <imachug@yandex.ru>
2018-09-05 07:44:04 +0300

    Add avatar support

--- main/src/router_pages/settings/settings.vue
+++ main/src/router_pages/settings/settings.vue
@@ -9,7 +9,11 @@
 			<option value="broadcast">ZeroNet only (slower)</option>
 		</select>
 
-		<button @click="save">Save</button>
+		<h2>Avatar</h2>
+		<input type="file" ref="avatar" @change="addAvatarFile" />
+		<button @click="removeAvatar" v-if="hasAvatar">Remove</button>
+
+		<button class="save" @click="save">Save</button>
 	</div>
 </template>
 
@@ -29,21 +33,23 @@
 		font-size: 20px
 		margin: 32px 0 16px
 
-	button
+	.save
 		display: block
 		margin: 48px auto 0
 </style>
 
 <script type="text/javascript">
 	import Users from "@/libs/users";
-	import {zeroPage, zeroAuth} from "@/zero";
+	import {zeroPage, zeroFS} from "@/zero";
 
 	export default {
 		name: "settings",
 		data() {
 			return {
 				username: "",
-				ipmode: ""
+				ipmode: "",
+				hasAvatar: false,
+				avatarExt: ""
 			};
 		},
 
@@ -52,16 +58,75 @@
 			const info = await Users.addressToInfo(authAddress);
 			this.username = info.username;
 			this.ipmode = info.ipmode || "broadcast";
+			this.hasAvatar = info.hasAvatar || false;
+			this.avatarExt = info.avatarExt || "";
 		},
 
 		methods: {
 			async save() {
+				if(this.hasAvatar) {
+					const file = this.$refs.avatar.files[0];
+					if(file) {
+						let ext = file.name.split(".").slice(-1)[0];
+						if(ext === "gz") {
+							// E.g. tar.gz or json.gz
+							ext = file.name.split(".").slice(-2).join(".");
+						}
+
+						// File to ArrayBuffer
+						const arrayBuffer = await new Promise((resolve, reject) => {
+							const fr = new FileReader();
+							fr.onload = () => {
+								resolve(fr.result);
+							};
+							fr.onerror = e => {
+								reject(e);
+							};
+							fr.readAsArrayBuffer(file);
+						});
+
+						// Write to disk
+						const directory = `data/users/${this.$store.state.siteInfo.auth_address}`;
+						await zeroFS.writeFile(
+							`${directory}/avatar.${ext}`,
+							arrayBuffer,
+							"arraybuffer"
+						);
+						const contentJson = JSON.parse(await zeroFS.readFile(`${directory}/content.json`));
+						contentJson.optional = ".*[.]png|.*[.]gif|.*[.]jpeg|.*[.]jpg|.*[.]piecemap[.]msgpack";
+						await zeroFS.writeFile(`${directory}/content.json`, JSON.stringify(contentJson, null, 1));
+					}
+				}
+
 				await Users.setInfo({
 					username: this.username,
-					ipmode: this.ipmode
+					ipmode: this.ipmode,
+					hasAvatar: this.hasAvatar,
+					avatarExt: this.avatarExt
 				});
 
 				this.$router.navigate("play");
+			},
+
+			removeAvatar() {
+				this.hasAvatar = false;
+				this.$refs.avatar.value = "";
+			},
+
+			addAvatarFile() {
+				const file = this.$refs.avatar.files[0];
+				if(file) {
+					this.hasAvatar = true;
+					const ext = file.name.split(".").slice(-1)[0].toLowerCase();
+					if(!["png", "gif", "jpeg", "jpg"].includes(ext)) {
+						zeroPage.alert("You can only use PNG, GIF and JP[E]G files for avatar");
+						this.removeAvatar();
+						return;
+					}
+					this.avatarExt = ext;
+				} else {
+					this.hasAvatar = false;
+				}
 			}
 		}
 	};
--- main/src/vue_components/user-avatar/user-avatar.vue
+++ main/src/vue_components/user-avatar/user-avatar.vue
@@ -1,21 +1,38 @@
 <template>
-	<div v-html="jdenticonAvatar" />
+	<div v-if="!hasAvatar" v-html="jdenticonAvatar" />
+	<img v-else :src="avatarSrc" :width="width || 64" :height="width || 64" />
 </template>
 
 <script type="text/javascript">
 	import jdenticon from "jdenticon";
+	import Users from "@/libs/users";
 
 	export default {
-		props: ["address"],
+		props: ["address", "width"],
 		name: "user-avatar",
 		data() {
 			return {
-				address: ""
+				address: "",
+				width: 0
 			};
 		},
 		computed: {
 			jdenticonAvatar() {
-				return jdenticon.toSvg(this.address, 64);
+				return jdenticon.toSvg(this.address, this.width || 64);
+			}
+		},
+		asyncComputed: {
+			async hasAvatar() {
+				if(!this.address) {
+					return false;
+				}
+
+				const info = await Users.addressToInfo(this.address);
+				return !!info.hasAvatar;
+			},
+			async avatarSrc() {
+				const info = await Users.addressToInfo(this.address);
+				return `data/users/${this.address}/avatar.${info.avatarExt}`;
 			}
 		}
 	}
